96e651cdc0fb5e3406c625634d8608be8916e089
jonathoncontact <jonathon.txt@gmail.com>
2026-09-18 18:59:12 -0700

    update FINAL

--- v2_with_jquery/chess.js
+++ v2_with_jquery/chess.js
@@ -1,72 +1,44 @@
-
-let startingBoard = [];
-let board = [];
-
 $(function () {
-	render_chessboard();
-	$("#move-button").on("click", move_piece);
-	$("#reset-button").on("click", reset_board);
+    render_chessboard();
+    $("#move-button").on("click", move_piece);
+    $("#reset-button").on("click", reset_board);
 });
 
-// Load the starting position from JSON and draw the board.
 function render_chessboard() {
-	$.getJSON("chessboard.json", function (data) {
-		startingBoard = [];
-		$.each(data.chessboard, function (_, row) {
-			startingBoard.push(Object.values(row));
-		});
-		board = [];
-		$.each(startingBoard, function (_, row) {
-			board.push(row.slice());
-		});
-		draw_chessboard();
-	});
-}
+    $.getJSON("chessboard.json", function (data) {
+        let table = $("<table>", { class: "chessboard" });
 
-// Draw the current board using jQuery-created elements.
-function draw_chessboard() {
-	const table = $("<table>", { class: "chessboard" });
+        $.each(data.chessboard, function (_, row) {
+            let table_row = $("<tr>");
+            $.each(row, function (square, piece) {
+                table_row.append($("<td>", { id: square }).html(piece));
+            });
+            table.append(table_row);
+        });
 
-	$.each(board, function (rowIndex, row) {
-		const tableRow = $("<tr>");
-		$.each(row, function (columnIndex, piece) {
-			const cell = $("<td>", {
-				id: String.fromCharCode(97 + columnIndex) + (8 - rowIndex)
-			});
-			cell.html(piece);
-			tableRow.append(cell);
-		});
-		table.append(tableRow);
-	});
-
-	$("#chessboard").empty().append(table);
+        $("#chessboard").empty().append(table);
+    });
 }
 
 function move_piece() {
-	const source = $("#src").val().toLowerCase();
-	const destination = $("#dst").val().toLowerCase();
-	const squarePattern = /^[a-h][1-8]$/;
-	if (!squarePattern.test(source) || !squarePattern.test(destination)) {
-		return false;
-	}
-	const sourceCell = $("#" + source);
-	const destinationCell = $("#" + destination);
+    let source = $("#src").val().toLowerCase();
+    let destination = $("#dst").val().toLowerCase();
+    let source_cell = $("#" + source);
+    let destination_cell = $("#" + destination);
 
-	if (source === destination || !sourceCell.length || !destinationCell.length ||
-		sourceCell.html() === "&nbsp;") {
-		return false;
-	}
+    if (!source_cell.length || !destination_cell.length || source === destination ||
+        source_cell.html() === "&nbsp;") {
+        return false;
+    }
 
-	destinationCell.html(sourceCell.html());
-	sourceCell.html("&nbsp;");
-	board[8 - Number(source[1])][source.charCodeAt(0) - 97] = "&nbsp;";
-	board[8 - Number(destination[1])][destination.charCodeAt(0) - 97] = destinationCell.html();
-	$("#src").val("");
-	$("#dst").val("");
-	return false;
+    destination_cell.html(source_cell.html());
+    source_cell.html("&nbsp;");
+    $("#src").val("");
+    $("#dst").val("");
+    return false;
 }
 
 function reset_board() {
-	window.location.reload();
-	return false;
+    window.location.reload();
+    return false;
 }
